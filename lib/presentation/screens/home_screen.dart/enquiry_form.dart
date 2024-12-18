48b327d98c21e30d7893e441acc667673feada83
prakharAKGa <srivastavaprakhar1007@gmail.com>
2024-12-18 21:36:55 +0530

    enquiry ui problem solved

--- lib/presentation/screens/home_screen.dart/enquiry_form.dart
+++ lib/presentation/screens/home_screen.dart/enquiry_form.dart
@@ -5,15 +5,15 @@ import 'package:my_zero_broker/bloc/Enquiry/enquiry_event.dart';
 import 'package:my_zero_broker/bloc/Enquiry/enquiry_state.dart';
 import 'package:my_zero_broker/presentation/widgets/ElevatedButton.dart';
 
-class EnquiryFormModal {
-  static void showEnquiryForm(BuildContext context, String subject ,String img) {
-    showModalBottomSheet(
+class EnquiryFormDialog {
+  static void showEnquiryForm(BuildContext context, String subject, String img) {
+    showDialog(
       context: context,
-      isScrollControlled: false,
+      barrierDismissible: true, // Allow dismiss by tapping outside the dialog
       builder: (BuildContext ctx) {
         return BlocProvider(
           create: (_) => EnquiryBloc(),
-          child: _EnquiryForm(subject: subject, img: img,),
+          child: _EnquiryForm(subject: subject, img: img),
         );
       },
     );
@@ -22,7 +22,7 @@ class EnquiryFormModal {
 
 class _EnquiryForm extends StatefulWidget {
   final String subject;
-   final String img;
+  final String img;
 
   const _EnquiryForm({required this.subject, required this.img});
 
@@ -31,7 +31,6 @@ class _EnquiryForm extends StatefulWidget {
 }
 
 class _EnquiryFormState extends State<_EnquiryForm> {
- 
   late EnquiryBloc _bloc;
 
   @override
@@ -45,115 +44,116 @@ class _EnquiryFormState extends State<_EnquiryForm> {
 
   @override
   Widget build(BuildContext context) {
-     final height = MediaQuery.of(context).size.height;
-      final width = MediaQuery.of(context).size.width;
-    return Wrap(
-      children: [
-        Stack(
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20), // Custom border radius for dialog
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent, // Transparent background for the dialog
+      child: SingleChildScrollView( // Make the content scrollable
+        child: Stack(
           clipBehavior: Clip.none,
           children: [
-            SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: const Color.fromARGB(255, 82, 94, 116), // Main background color
-                  borderRadius: const BorderRadius.vertical(
-                    top: Radius.circular(30),
-                  ),
-                  border: const Border(
-                    top: BorderSide(
-                      color: Colors.white, // The color of the border
-                      width: 10.0, // The width of the border
-                      style: BorderStyle.solid, // The style of the border (solid)
-                    ),
+            Container(
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 82, 94, 116),
+                borderRadius: BorderRadius.circular(30),
+                border: const Border(
+                  top: BorderSide(
+                    color: Colors.white,
+                    width: 10.0,
+                    style: BorderStyle.solid,
                   ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: GestureDetector(
-                        onTap: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Icon(
-                          Icons.cancel,
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    // Subject Section
-                    Text(
-                      'Enquiry Form',
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      '${widget.subject}',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white70,
-                      ),
-                    ),
-                    SizedBox(height: 20),
-                    // Text Fields
-                    _buildTextField(
-                      label: 'Name',
-                      onChanged: (value) => _bloc.add(NameChanged(value)),
-                    ),
-                    _buildTextField(
-                      label: 'Email',
-                      onChanged: (value) => _bloc.add(EmailChanged(value)),
-                    ),
-                    _buildTextField(
-                      label: 'Phone Number',
-                      onChanged: (value) => _bloc.add(PhoneNumberChanged(value)),
-                    ),
-                    _buildTextField(
-                      label: 'Your Query',
-                      onChanged: (value) => _bloc.add(QueryChanged(value)),
-                    ),
-                    SizedBox(height: 20),
-                    _buildSubmitButton(_bloc),
-                    BlocBuilder<EnquiryBloc, EnquiryState>(
-                      builder: (context, state) {
-                        if (state.status == EnquiryStatus.loading) {
-                          return Center(child: CircularProgressIndicator());
-                        } else if (state.status == EnquiryStatus.success) {
-                          return _buildMessage(state.message, Colors.green);
-                        } else if (state.status == EnquiryStatus.error) {
-                          return _buildMessage(state.message, Colors.red);
-                        }
-                        return SizedBox.shrink();
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.pop(context);
                       },
+                      child: const Icon(
+                        Icons.cancel,
+                        color: Colors.red,
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 20),
+                  // Subject Section
+                  Text(
+                    'Enquiry Form',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '${widget.subject}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white70,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Text Fields
+                  _buildTextField(
+                    label: 'Name',
+                    onChanged: (value) => _bloc.add(NameChanged(value)),
+                  ),
+                  _buildTextField(
+                    label: 'Email',
+                    onChanged: (value) => _bloc.add(EmailChanged(value)),
+                  ),
+                  _buildTextField(
+                    label: 'Phone Number',
+                    onChanged: (value) => _bloc.add(PhoneNumberChanged(value)),
+                  ),
+                  _buildTextField(
+                    label: 'Your Query',
+                    onChanged: (value) => _bloc.add(QueryChanged(value)),
+                  ),
+                  SizedBox(height: 20),
+                  _buildSubmitButton(_bloc),
+                  BlocBuilder<EnquiryBloc, EnquiryState>(
+                    builder: (context, state) {
+                      if (state.status == EnquiryStatus.loading) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (state.status == EnquiryStatus.success) {
+                        return _buildMessage(state.message, Colors.green);
+                      } else if (state.status == EnquiryStatus.error) {
+                        return _buildMessage(state.message, Colors.red);
+                      }
+                      return SizedBox.shrink();
+                    },
+                  ),
+                ],
               ),
             ),
             Positioned(
-              top: -40, // Adjust position of the avatar
-              left: MediaQuery.of(context).size.width / 2 - 40, // Center the avatar
+              top: -40,
+              left: MediaQuery.of(context).size.width / 2 - 80,
               child: CircleAvatar(
                 radius: 40,
                 backgroundColor: Colors.blueAccent,
                 child: CircleAvatar(
                   radius: 35,
                   backgroundColor: const Color(0xFF042b59),
-                  child: Image.asset('${widget.img}'), // Your logo here
+                  child: Image.asset('${widget.img}'),
                 ),
               ),
             ),
           ],
         ),
-      ],
+      ),
     );
   }
 
@@ -161,13 +161,15 @@ class _EnquiryFormState extends State<_EnquiryForm> {
     required String label,
     required Function(String) onChanged,
   }) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8.0),
       child: TextField(
-         style: TextStyle(
-    color: Colors.white,
-    fontWeight: FontWeight.bold // Change text color here
-  ),
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
         onChanged: onChanged,
         decoration: InputDecoration(
           labelText: label,
@@ -182,24 +184,23 @@ class _EnquiryFormState extends State<_EnquiryForm> {
           ),
           prefixIcon: Icon(Icons.text_fields, color: Colors.white),
         ),
-         cursorColor: Colors.red,
+        cursorColor: Colors.red,
       ),
     );
   }
 
   Widget _buildSubmitButton(EnquiryBloc bloc) {
-     final height = MediaQuery.of(context).size.height;
-      final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Elevatedbutton(
       text: 'Submit',
-      height:height * 0.8,
-  width:width,
-  bgcolor:const Color.fromARGB(255, 206, 49, 21),
- foregroundColor:Colors.white,
+      height: height * 0.8, // Adjust height to avoid overflow
+      width: width * 0.8, // Adjust width to fit in the dialog
+      bgcolor: const Color.fromARGB(255, 206, 49, 21),
+      foregroundColor: Colors.white,
       onPressed: () {
         bloc.add(SubmitEnquiry());
       },
-      
     );
   }
 
@@ -208,7 +209,8 @@ class _EnquiryFormState extends State<_EnquiryForm> {
       padding: const EdgeInsets.symmetric(vertical: 16),
       child: Text(
         message,
-        style: TextStyle(fontSize: 16, color: color),
+        style: TextStyle(fontSize: 16, color: color,
+        fontWeight: FontWeight.bold),
       ),
     );
   }
