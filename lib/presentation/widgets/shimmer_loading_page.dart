ed7ab1bbd7087f42572231deb6d7709be014f007
prakharAKGa <srivastavaprakhar1007@gmail.com>
2024-12-29 19:13:44 +0530

    shrimmer effect resolved

--- lib/presentation/widgets/shimmer_loading_page.dart
+++ lib/presentation/widgets/shimmer_loading_page.dart
@@ -8,66 +8,68 @@ class ShimmerLoadingPage extends StatelessWidget {
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(height: 50),
-            // Shimmer for title
-            Shimmer.fromColors(
-              baseColor: Colors.grey[300]!,
-              highlightColor: Colors.grey[100]!,
-              child: Container(
-                height: 30,
-                width: 200,
-                color: Colors.grey[300],
-              ),
-            ),
-            SizedBox(height: 20),
-            // Shimmer for logo
-            Shimmer.fromColors(
-              baseColor: Colors.grey[300]!,
-              highlightColor: Colors.grey[100]!,
-              child: Container(
-                height: 100,
-                width: 100,
-                color: Colors.grey[300],
-              ),
-            ),
-            SizedBox(height: 30),
-            // Shimmer for buttons
-            for (int i = 0; i < 4; i++) ...[
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 50),
+              // Shimmer for title
               Shimmer.fromColors(
                 baseColor: Colors.grey[300]!,
                 highlightColor: Colors.grey[100]!,
                 child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
+                  height: 30,
+                  width: 200,
+                  color: Colors.grey[300],
                 ),
               ),
-            ],
-            SizedBox(height: 20),
-            // Shimmer for dropdowns
-            for (int i = 0; i < 5; i++) ...[
+              SizedBox(height: 20),
+              // Shimmer for logo
               Shimmer.fromColors(
                 baseColor: Colors.grey[300]!,
                 highlightColor: Colors.grey[100]!,
                 child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  margin: const EdgeInsets.symmetric(vertical: 8),
-                  decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
+                  height: 100,
+                  width: 100,
+                  color: Colors.grey[300],
                 ),
               ),
+              SizedBox(height: 30),
+              // Shimmer for buttons
+              for (int i = 0; i < 4; i++) ...[
+                Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ],
+              SizedBox(height: 20),
+              // Shimmer for dropdowns
+              for (int i = 0; i < 5; i++) ...[
+                Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(vertical: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ],
             ],
-          ],
+          ),
         ),
       ),
     );
