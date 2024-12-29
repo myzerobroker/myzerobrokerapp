916c64d2456acdbe14196cd8c76cdc58c933c4b0
Ap00rvx <apurvabraj419@gmail.com>
2024-12-29 12:49:46 +0530

    shimmer loading

---
+++ lib/presentation/screens/my_listing_page/my_listing_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MyListingPage extends StatefulWidget {
+  const MyListingPage({super.key});
+
+  @override
+  State<MyListingPage> createState() => _MyListingPageState();
+}
+
+class _MyListingPageState extends State<MyListingPage> {
+  String url = "https://www.myzerobroker.com/api/user/my-listings"; 
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/widgets/shimmer_loading_page.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ShimmerLoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            // Shimmer for title
+            Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                height: 30,
+                width: 200,
+                color: Colors.grey[300],
+              ),
+            ),
+            SizedBox(height: 20),
+            // Shimmer for logo
+            Shimmer.fromColors(
+              baseColor: Colors.grey[300]!,
+              highlightColor: Colors.grey[100]!,
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.grey[300],
+              ),
+            ),
+            SizedBox(height: 30),
+            // Shimmer for buttons
+            for (int i = 0; i < 4; i++) ...[
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+            SizedBox(height: 20),
+            // Shimmer for dropdowns
+            for (int i = 0; i < 5; i++) ...[
+              Shimmer.fromColors(
+                baseColor: Colors.grey[300]!,
+                highlightColor: Colors.grey[100]!,
+                child: Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: const EdgeInsets.symmetric(vertical: 8),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}
