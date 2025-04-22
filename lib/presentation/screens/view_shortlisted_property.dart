12403b1c7267147d43327a79b51c915fb4711479
Ap00rvx <apurvabraj419@gmail.com>
2025-04-22 14:47:13 +0530

    ui changes of listing and shortlisted page

--- lib/presentation/screens/view_shortlisted_property.dart
+++ lib/presentation/screens/view_shortlisted_property.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:my_zero_broker/data/area_details_dependency.dart';
 import 'package:my_zero_broker/data/models/property_in_cities_model.dart';
 import 'package:my_zero_broker/locator.dart';
+import 'package:my_zero_broker/presentation/screens/view_property_in_city_page/property_detail_screen.dart';
+import 'package:my_zero_broker/presentation/widgets/image_carousel.dart';
+import 'package:my_zero_broker/utils/constant/theme.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ViewShortlistedProperty extends StatefulWidget {
@@ -91,190 +94,96 @@ class _ViewShortlistedPropertyState extends State<ViewShortlistedProperty> {
                           .first["a_name"];
                       print(areas);
                       print(area);
-                      return Center(
-                        child: Container(
-                          width: double.infinity,
-                          margin: EdgeInsets.all(12),
-                          padding: EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 4,
-                                spreadRadius: 1,
-                                offset: Offset(0, 4),
-                              ),
-                            ],
+                      return Container(
+                        margin: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: ColorsPalette.cardBgColor,
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(
+                            color: ColorsPalette.primaryColor.withOpacity(0.3),
+                            width: 1,
                           ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              // Header: Logo & Title
-                              photos.isEmpty
-                                  ? Image.asset(
-                                      "assets/images/my_zero_broker_logo (2).png")
-                                  : Image.network(
-                                      "https://myzerobroker.com/public/storage/" +
-                                          photos[0].toString(),
-                                      height: 300,
-                                      width: double.infinity,
-                                      fit: BoxFit.fitHeight,
-                                      loadingBuilder:
-                                          (context, child, loadingProgress) {
-                                        if (loadingProgress == null) {
-                                          return child;
-                                        }
-                                        return Center(
-                                            child: CircularProgressIndicator());
-                                      },
-                                    ),
-                              Divider(color: Colors.black45),
-                              SizedBox(height: 6),
-                              // Property Details
-                              Wrap(
-                                spacing: 10,
-                                runSpacing: 6,
+                        ),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    PropertyDetailScreen(property: property),
+                              ),
+                            );
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.all(14.0),
+                            child: Card(
+                              color: ColorsPalette.cardBgColor,
+                              margin: EdgeInsets.symmetric(vertical: 8.0),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              elevation: 0,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  Row(
-                                    children: [
-                                      FutureBuilder(
-                                          future: checkIfStringexist(
-                                              jsonEncode(property.toJson())),
-                                          builder: (context, sp) {
-                                            if (sp.hasData) {
-                                              if (sp.data! == false) {
-                                                return IconButton(
-                                                  icon: Icon(
-                                                      Icons.favorite_border),
-                                                  onPressed: () async {
-                                                    SharedPreferences prefs =
-                                                        await SharedPreferences
-                                                            .getInstance();
-                                                    final list =
-                                                        prefs.getStringList(
-                                                                "favList") ??
-                                                            [];
-                                                    list.add(jsonEncode(
-                                                        property.toJson()));
-                                                    prefs.setStringList(
-                                                        "favList", list);
-                                                    setState(() {});
-                                                  },
-                                                );
-                                              } else {
-                                                return IconButton(
-                                                  icon: Icon(
-                                                    Icons.favorite,
-                                                    color: Colors.red,
-                                                  ),
-                                                  onPressed: () async {
-                                                    SharedPreferences prefs =
-                                                        await SharedPreferences
-                                                            .getInstance();
-                                                    final list =
-                                                        prefs.getStringList(
-                                                                "favList") ??
-                                                            [];
-                                                    list.remove(jsonEncode(
-                                                        property.toJson()));
-                                                    prefs.setStringList(
-                                                        "favList", list);
-                                                    setState(() {});
-                                                  },
-                                                );
-                                              }
-                                            } else {
-                                              return Container();
-                                            }
-                                          })
-                                    ],
+                                  photos.isEmpty
+                                      ? Image.asset(
+                                          "assets/images/my_zero_broker_logo (2).png",
+                                          height: 200,
+                                          fit: BoxFit.cover)
+                                      : photos.length > 1
+                                          ? ImageCarousel(
+                                              images: photos,
+                                            )
+                                          : Image.network(
+                                              "https://myzerobroker.com/public/storage/" +
+                                                  photos[0].toString(),
+                                              height: 200,
+                                              width: double.infinity,
+                                              fit: BoxFit.cover,
+                                              loadingBuilder: (context, child,
+                                                  loadingProgress) {
+                                                if (loadingProgress == null)
+                                                  return child;
+                                                return Center(
+                                                    child:
+                                                        CircularProgressIndicator());
+                                              },
+                                            ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          loc + ", " + area,
+                                          style: TextStyles.bodyStyle.copyWith(
+                                              color: ColorsPalette
+                                                  .textSecondaryColor),
+                                        ),
+                                        Text(
+                                          property.status == 'Rent'
+                                              ? 'Rent: ${formatPrice(property.expectedRent.toString())}'
+                                              : 'Price: ${formatPrice(property.expectedPrice.toString())}',
+                                          style: TextStyles.priceStyle,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Icon(Icons.star,
+                                                color: ColorsPalette.starColor,
+                                                size: 16),
+                                            Text("4.5",
+                                                style: TextStyles.bodyStyle),
+                                            Text(" For ${property.status}",
+                                                style: TextStyles.bodyStyle),
+                                          ],
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                  _detailRow(
-                                      'Property No:',
-                                      "RS" + property.id.toString(),
-                                      Colors.blue),
-                                  _detailRow(
-                                      'Posted on:',
-                                      DateTime.parse(
-                                                  property.createdAt.toString())
-                                              .day
-                                              .toString() +
-                                          "-" +
-                                          DateTime.parse(
-                                                  property.createdAt.toString())
-                                              .month
-                                              .toString() +
-                                          "-" +
-                                          DateTime.parse(
-                                                  property.createdAt.toString())
-                                              .year
-                                              .toString(),
-                                      Colors.red),
-                                  _detailRow('Location:', loc, Colors.red),
-                                  _detailRow(
-                                      'Area:', area.toString(), Colors.red),
                                 ],
                               ),
-                              SizedBox(height: 10),
-                              _detailRow(
-                                  'Plot Area:',
-                                  property.areaSqft.toString() == "null"
-                                      ? "Not Defined"
-                                      : (property.areaSqft.toString() +
-                                          ' sqFT')),
-                              _detailRow(
-                                  'Built-Up Area:',
-                                  property.carpetAreaSqft.toString() == "null"
-                                      ? "Not Defined"
-                                      : (property.carpetAreaSqft.toString() +
-                                          ' sqFT')),
-                              _detailRow('Property Age:',
-                                  property.propertyAge.toString()),
-                              _detailRow(
-                                  'Floors:', property.totalFloor.toString()),
-                              _detailRow('Facing:', property.facing.toString()),
-                              _detailRow(
-                                  'Offer:',
-                                  "₹" + property.expectedPrice.toString(),
-                                  Colors.green),
-                              _detailRow('Maintainance:',
-                                  '₹' + property.maintenanceCost.toString()),
-                              _detailRow('Furnishing:',
-                                  property.furnishing.toString()),
-                              _detailRow(
-                                  'Parking:', property.parkingType.toString()),
-                              _detailRow('Kitchen Type:',
-                                  property.kitchenType.toString()),
-                              _detailRow(
-                                  'Bathrooms:', property.bathroom.toString()),
-                              _detailRow(
-                                  'Balcony:', property.balcony.toString()),
-                              SizedBox(height: 6),
-                              Text(
-                                'No extra amenities',
-                                style: TextStyle(
-                                  fontStyle: FontStyle.italic,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                              SizedBox(height: 12),
-                              ElevatedButton(
-                                onPressed: () {
-                                  // Action on press
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.black,
-                                  foregroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(8),
-                                  ),
-                                  minimumSize: Size(double.infinity, 40),
-                                ),
-                                child: Text('Get Owner Details'),
-                              ),
-                            ],
+                            ),
                           ),
                         ),
                       );
@@ -287,6 +196,32 @@ class _ViewShortlistedPropertyState extends State<ViewShortlistedProperty> {
         },
       ),
     );
+  }
+}
+
+String formatPrice(String price) {
+  try {
+    double priceValue = double.parse(price);
+    String formattedPrice;
+
+    if (priceValue >= 10000000) {
+      formattedPrice =
+          (priceValue / 10000000).toStringAsFixed(1).replaceAll('.0', '') +
+              ' Cr';
+    } else if (priceValue >= 100000) {
+      formattedPrice =
+          (priceValue / 100000).toStringAsFixed(1).replaceAll('.0', '') +
+              ' Lakh';
+    } else if (priceValue >= 1000) {
+      formattedPrice =
+          (priceValue / 1000).toStringAsFixed(1).replaceAll('.0', '') + ' K';
+    } else {
+      formattedPrice = priceValue.toStringAsFixed(0);
+    }
+
+    return '₹$formattedPrice (Negotiable)';
+  } catch (e) {
+    return '₹$price (Negotiable)';
   }
 }
 
